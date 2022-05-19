5cc0855a2a1b5c0f685e65d06fa5688bb4ae4a39
pratpaliwal <pratpaliwal@USGURPRAT01.us.deloitte.com>
2022-05-19 13:55:47 +0530

    First commit

---
+++ src/main/java/NewFramework/NewFramework/HelloFramework.java
@@ -0,0 +1,10 @@
+package NewFramework.NewFramework;
+
+public class HelloFramework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Hello Framework!" );
+	}
+
+}
